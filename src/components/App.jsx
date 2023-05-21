afca6b7e70ffc1e0a578b5c7efd7c118d42ee5ae
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-05-21 14:03:29 +0300

    refactor code on react hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,46 @@
-import { Component } from "react";
+import { useState } from "react";
 import Section from "./Section/Section";
 import Statistics from "./Statistics/Statistics";
 import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
 
-export default class App extends Component {
-  state = {
+export default function App() {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0
+  });
+
+  const handleClick = (key) => {
+    setState(prevState => { 
+      return {...prevState, [key]: prevState[key] + 1}})  
   }
 
-  handleClick = (key) => {
-    this.setState(prevState => { 
-      return {[key]: prevState[key] + 1}})  
+  const countTotalFeedback = () => {
+    return Object.values(state).reduce((acc, el) => acc + el, 0 )
+}
+
+  const countPositiveFeedbackPercentage = () => {
+      return Math.round(100 / (countTotalFeedback()) * state.good)
   }
 
-  countTotalFeedback = () => {
-      return Object.values(this.state).reduce((acc, el) => acc + el, 0 )
-  }
-
-
-  countPositiveFeedbackPercentage = () => {
-      return Math.round(100 / (this.countTotalFeedback()) * this.state.good)
-  }
-
-  render () {
-    const options = Object.keys(this.state);
-    return (
+  return (
     <>
     <Section 
       title="Please leave feedback"
       children={
         <FeedbackOptions 
-          props={options}
-          handleClick={this.handleClick}
+          props={Object.keys(state)}
+          handleClick={handleClick}
         />}
     />
 
     <Section title="Statistics" children={
       < Statistics 
-        props={this.state}
-        total={this.countTotalFeedback}
-        percentage={this.countPositiveFeedbackPercentage}
+        props={state}
+        total={countTotalFeedback}
+        percentage={countPositiveFeedbackPercentage}
       />}
     />
     </>
     )
-  }
-
-
 }
